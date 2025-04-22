613f0e4f36271a55c7316fd7dbebde36ef6a93a7
Nilesh Pandey <115298747+nilesh13github@users.noreply.github.com>
2025-04-22 21:51:09 +0530

    added percentage showing in ai text detection

--- inference.py
+++ inference.py
@@ -6,10 +6,21 @@ vectorizer = joblib.load('vectorizer.pkl')
 
 
 def detector(text):
-    email_vec = vectorizer.transform([text])
-    pred = classifier.predict(email_vec)
 
-    if pred[0] == 1:
-        return "This message is AI Generated"
-    else:
-        return "This message is Human Written"
+    chunks = text.split(". ")
+    total_count = len(chunks)
+    ai_count = 0
+    for sent in chunks:
+        email_vec = vectorizer.transform([sent])
+        pred = classifier.predict(email_vec)
+
+
+
+        if pred[0] == 1:
+            ai_count = ai_count+1
+        else:
+            continue
+
+    total_ai_percentage = ((ai_count/total_count)*100)
+
+    return f"{round(total_ai_percentage, 2)} % AI GENERATED"
