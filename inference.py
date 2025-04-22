1312ec57b061bf6d346996f826afca2538534aff
Nilesh Pandey <115298747+nilesh13github@users.noreply.github.com>
2025-04-22 22:24:55 +0530

    Update inference.py

--- inference.py
+++ inference.py
@@ -18,9 +18,13 @@ def detector(text):
 
         if pred[0] == 1:
             ai_count = ai_count+1
+            text = text.replace(sent, f"""<span class="ai-text">{sent}</span>""")
         else:
             continue
 
     total_ai_percentage = ((ai_count/total_count)*100)
 
-    return f"{round(total_ai_percentage, 2)} % AI Generated"
+    #print(text)
+
+    return f"{round(total_ai_percentage, 2)} % AI Generated", text
+
